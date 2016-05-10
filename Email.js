9b1d94c3f3f0ffed00d1888fcb319aab9576a24a
thomasgengel34 <thomasgengel34@yahoo.com>
2016-05-10 16:05:49 -0500

    in progress

--- Email.js
+++ Email.js
@@ -14,105 +14,35 @@ window.onload = function () {
         $("#pCats").toggle("explode");
     });
 
-    $('#btnSubmit').on("click", bhme.sendEmail);
+    $('#btnSimpleSubmit').on("click", bhme.sendSimpleEmail);
 
 }
 
-bhme.sendEmail = function () {
-    var subject = 'Email%20from%20the%20Website'; 
-    var firstName = encodeURIComponent($('#tbFirstName').val()); 
-    var body = "Name:  " + firstName + " " + encodeURIComponent($('#tbLastName').val());
-    var spacer = "  ";
-    body += spacer + "  Email: " + ($('#tbEmail').val());
-    body += spacer + "Parent Name:  " + encodeURIComponent($('#tbParentFirstName').val()) + " " + encodeURIComponent($('#tbParentLastName').val());
-    body += spacer + "Parent Email: " + encodeURIComponent($('#tbParentEmail').val());
-
+bhme.sendSimpleEmail = function () {
+    var subject = 'Simple%20Email%20from%20the%20Website';
+    var name = "No name supplied.";
+    if ($('#tbSimpleName').val().length>1) {
+       name = encodeURIComponent($('#tbSimpleName').val());
+    } 
+    var body = "Name:  " + name;   
     var phone ="not filled out";
-    if ($('#tbPhone').length>1){
-        phone=encodeURIComponent($('#tbPhone').val());
+    if ($('#tbSimplePhone').val().length>1){
+        phone= $('#tbSimplePhone').val();
     }  
-    body += spacer + "Telephone: " + phone;
-     
-          var isMobile = "Mobile Phone checkbox is not checked.";
-          if ($('#ckIsMobile').is(':checked')) {
-              isMobile = "Mobile Phone checkbox is checked.";
-          } 
-          body +=spacer + isMobile;
+    body +=  "  Telephone: " + phone; 
 
-          var isAdult= "Filled out by adult checkbox is not checked."; 
-          if ($('#ckIsAdult').is(':checked')) {
-              isAdult = "Filled out by adult checkbox is checked.";
-          } 
-          body += "  " + isAdult;
+    var tbSimpleComments = $('#tbSimpleComments').val();  
 
-          var ckComService="";
-          if ($('#ckComService').is(':checked')) {
-              ckComService = "Community service checkbox is checked.";
-          } 
-          body += "  " + ckComService;
-
-          var ckScoutService;
-          if ($('#ckScoutService').is(':checked')) {
-              ckScoutService = "Scout service checkbox is checked.";
-          }
-          else ckScoutService = "";
-          body += "  " + ckScoutService;
-
-          var ckEagleScoutService;
-          if ($('#ckEagleScoutService').is(':checked')) {
-              ckEagleScoutService = "Eagle Scout service checkbox is checked.";
-          }
-          else ckEagleScoutService = "";
-          body += "  " + ckEagleScoutService;
-
-          var ckNewsletter;
-          if ($('#ckNewsletter').is(':checked')) {
-              ckNewsletter = "Newsletter checkbox is checked.";
-          }
-          else ckNewsletter = "";
-          body += "  " + ckNewsletter;
-
-          var ckArtContests;
-          if ($('#ckArtContests').is(':checked')) {
-              ckArtContests = "Art Contests checkbox is checked.";
-          }
-          else ckArtContests = "";
-          body += "  " + ckArtContests;
-
-          var ckClassInfo;
-          if ($('#ckClassInfo').is(':checked')) {
-              ckClassInfo = "Class Info checkbox is checked.";
-          }
-          else ckClassInfo = "";
-          body += "  " + ckClassInfo;
-
-          var ckRegistering;
-          if ($('#ckRegistering').is(':checked')) {
-              ckRegistering = "Registering checkbox is checked.";
-          }
-          else ckRegistering = "";
-          body += "  " + ckRegistering;
-
-          var tbComments = $('#tbComments').val();
-          if (tbComments.length < 1) {
-              tbComments = "No Comments";
-          }
-
-          body += spacer+spacer+ "  Comments: " + tbComments;
-          
-          body += "yada yada yada.";
-         //body += "   You may see some odd characters in some of the above.  For example, a last name with a space in it:  von Zuts  will appear as von Zuts . This is a security feature.  Due to some new security features in email providers, it is now difficult to impossible to format emails in code.  The webmaster regrets this.  ";
-
-    //      //todo: put in a second check - if there is just personal info in the first column and no checkboxes or comments, don't send an email. Send back a nice message telling them we can't find anything checked or commented. 
+          body +=  "  Comments: " + tbSimpleComments;
+           
+        body += "   You may see some odd characters in some of the above.  For example, a last name with a space in it:  von Zuts  will appear as von Zuts . This is a security feature.  Due to some new security features in email providers, it is now difficult to impossible to format emails in code.  The webmaster regrets this.  ";
  
 
     var uri = 'mailto:thomasgengel34@yahoo.com';
     uri += '?subject=' + (subject);
-    uri += '&body=' + body;
-    //      window.location.assign(uri);
+    uri += '&body=' + body; 
     $('#lblResponse').text('Thank you.  Your email has been sent!');
-    $('#emailForm').attr("action", uri)
-    //   }
+    $('#emailForm').attr("action", uri) 
 }
 
 bhme.ClearFields = function () {
