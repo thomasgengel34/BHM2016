96957b5002241d7ebba5366130da61acbffa1497
thomasgengel34 <thomasgengel34@yahoo.com>
2016-05-21 13:27:23 -0500

    Kids in Progress

---
+++ Kids.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../jquery-1.10.2.js" />
+/// <reference path="../jquery-ui-1.11.4.js" />
+
+/// <reference path="../modernizr-2.6.2.js" />
+
+window.onload = function () {  
+    $("#ckSquirrel").change(function () {
+        $(".zufeed").toggle("slow");
+    });
+    $("#ckCats").click(function() {
+  $( "#pCats" ).toggle( "explode" );
+});
+}
