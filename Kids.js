cc50b9b1a4454cc1c32d1d32523ff6b014e0cd4a
thomasgengel34 <thomasgengel34@yahoo.com>
2016-05-28 12:35:28 -0500

    in progress

--- Kids.js
+++ Kids.js
@@ -7,7 +7,11 @@
 "use strict";
 var bhmKids= {};
 
-window.onload = function () {   
-   
+window.onload = function () {
+    // not working
+    $("li.right input[type='checkbox']").on("click", function() {
+        $(this).toggle(css('color','green'));
+    });
+    $("li.right input[type='checkbox']").on("click", function () { alert("Hello, World!"); });
 }
   
