1e010cedc0d1750ae1f383f5efdc099f3064f4ea
thomasgengel34 <thomasgengel34@yahoo.com>
2016-06-07 09:36:56 -0500

    Add files via upload

---
+++ default.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-ui-1.11.4.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="respond.js" />  
+/// <reference path="modernizr-2.6.2.js" />
+"use strict";
+
+//leave for testing
+var greeting = 'Hello World';
+
+var bhmDefault = {};
+
+window.onload = function () {
+    bhmDefault.Year = new Date().getFullYear();
+    $('.timeSpan').text(bhmDefault.Year);
+}
