c0ce21bf936c1b850644f3b1fc3b4e852c748d5b
thomasgengel34 <thomasgengel34@yahoo.com>
2016-06-13 10:38:35 -0500

    Hands On History in process

---
+++ HandsOnHistory.js
@@ -0,0 +1,85 @@
+﻿/// <reference path="../_references.js" /> 
+"use strict";
+var bhmHoh={};
+bhmHoh.chip = document.getElementById("chip");
+bhmHoh.chip.addEventListener("dragstart", function () { window.event.dataTransfer.setData("Text", this.id); });
+bhmHoh.b1 = document.getElementById("bucket1");
+bhmHoh.b1.addEventListener("dragenter", function () {
+    bhmHoh.b1.classList.add("over");
+    window.event.returnValue = false;
+});
+bhmHoh.b1.addEventListener("dragleave", function () {
+    bhmHoh.b1.classList.remove("over");
+});
+bhmHoh.b1.addEventListener("dragover", function () {
+    window.event.returnValue = false;
+});
+bhmHoh.b1.addEventListener("drop", function () {
+    window.event.returnValue = false;
+    var data = event.dataTransfer.getData("Text");
+    var d = document.getElementById(data);
+    d.classList.remove("begin");
+    d.classList.add("dropped");
+    this.appendChild(d);
+});
+
+bhmHoh.b2 = document.getElementById("bucket2");
+bhmHoh.b2.addEventListener("dragenter", function () {
+    bhmHoh.b2.classList.add("over");
+    window.event.returnValue = false;
+});
+bhmHoh.b2.addEventListener("dragleave", function () {
+    bhmHoh.b2.classList.remove("over");
+});
+bhmHoh.b2.addEventListener("dragover", function () {
+    window.event.returnValue = false;
+});
+bhmHoh.b2.addEventListener("drop", function () {
+    window.event.returnValue = false;
+    var data = event.dataTransfer.getData("Text");
+    var d = document.getElementById(data);
+    d.classList.remove("begin");
+    d.classList.add("dropped");
+    this.appendChild(d);
+});
+
+bhmHoh.b3 = document.getElementById("bucket3");
+bhmHoh.b3.addEventListener("dragenter", function () {
+    bhmHoh.b3.classList.add("over");
+    window.event.returnValue = false;
+});
+bhmHoh.b3.addEventListener("dragleave", function () {
+    bhmHoh.b3.classList.remove("over");
+});
+bhmHoh.b3.addEventListener("dragover", function () {
+    window.event.returnValue = false;
+});
+bhmHoh.b3.addEventListener("drop", function () {
+    window.event.returnValue = false;
+    var data = event.dataTransfer.getData("Text");
+    var d = document.getElementById(data);
+    d.classList.remove("begin");
+    d.classList.add("dropped");
+    this.appendChild(d);
+});
+
+
+bhmHoh.b4 = document.getElementById("bucket4");
+bhmHoh.b4.addEventListener("dragenter", function () {
+    bhmHoh.b4.classList.add("over");
+    window.event.returnValue = false;
+});
+bhmHoh.b4.addEventListener("dragleave", function () {
+    bhmHoh.b4.classList.remove("over");
+});
+bhmHoh.b4.addEventListener("dragover", function () {
+    window.event.returnValue = false;
+});
+bhmHoh.b4.addEventListener("drop", function () {
+    window.event.returnValue = false;
+    var data = event.dataTransfer.getData("Text");
+    var d = document.getElementById(data);
+    d.classList.remove("begin");
+    d.classList.add("dropped");
+    this.appendChild(d);
+});
